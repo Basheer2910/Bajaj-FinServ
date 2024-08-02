e83fcd69de74b85f983b637b11e2989a3064e448
Basheer2910 <syed_basheer@srmap.edu.in>
2024-08-02 19:49:52 +0530

    add vercel.json

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,9 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
+app.get('/',()=>{
+    res.json("Working!!");
+})
 app.post('/bfhl', (req, res) => {
     try {
 
