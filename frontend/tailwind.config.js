515361e2bb271ae3cd40b6f86b854ddd6ce9cebf
Basheer2910 <127715338+Basheer2910@users.noreply.github.com>
2024-08-02 20:56:50 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./Components/**/*.{js,ts,jsx,tsx}"],
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
   theme: {
     extend: {},
   },
